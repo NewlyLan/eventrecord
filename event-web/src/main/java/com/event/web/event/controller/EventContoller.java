c9da3a01632d6089c4204c8094d64783093b19bc
NewlyLan <826550251@qq.com>
2020-10-25 21:58:11 +0800

    给工程配置统一日志

--- event-web/src/main/java/com/event/web/event/controller/EventContoller.java
+++ event-web/src/main/java/com/event/web/event/controller/EventContoller.java
@@ -3,6 +3,8 @@ package com.event.web.event.controller;
 import com.event.entry.Account;
 import com.event.web.event.service.AccountService;
 import org.apache.ibatis.annotations.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class EventContoller {
+    Logger logger = LoggerFactory.getLogger(EventContoller.class);
+
     @Autowired
     AccountService accountService;
     @RequestMapping("/hello")
     public Account getAccount(@Param("accountNo")String accountNo){
+        logger.info("=====进入查询账号=======");
         return accountService.selectByPrimaryKey(accountNo);
     }
 }
